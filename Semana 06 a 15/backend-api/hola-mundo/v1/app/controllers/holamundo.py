af2e09c73d007a741e81d9fbc91edd502cc95597
Daniel BG. <docente.daniel.cr@gmail.com>
2026-06-15 16:44:47 -0600

    actualiza metodo saludar

--- Semana 06 a 15/backend-api/hola-mundo/v1/app/controllers/holamundo.py
+++ Semana 06 a 15/backend-api/hola-mundo/v1/app/controllers/holamundo.py
@@ -13,15 +13,11 @@ def obtener_ventas():
 # http://127.0.0.1:5000/holamundo-ejemplo/api/v1/saludo?nombre=Jose&apellido=Mujica
 @holamundo_endpoints.route('/saludo', methods=['GET'])
 def saludar():
-    # obtenemos los parametros
-    nombre = request.args.get('nombre')
-    apellido = request.args.get('apellido')
-    apellido2 = request.args.get('apellido2')
-    # Valida los parametros
+    data = request.get_json()
+    nombre = data.get('nombre')
+    apellido = data.get('apellido')
     if not nombre or not apellido:
         return jsonify({'error': 'Faltan parámetros nombre o apellido'}), 400
 
-    # Crea un saludo.
-    saludo = f"Hola {nombre} {apellido} {apellido2} !"
-    # Convierte el saludo a JSON
-    return jsonify({'saludo': saludo}), 200 # la se convierte a JSON
+    saludo = f"Hola {nombre} {apellido}"
+    return jsonify({'saludo': saludo}), 200
